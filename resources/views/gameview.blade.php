1df1d1102a7197754a9f89dbf2837d631cca0ac0
Joe74012 <1210764@student.roc-nijmegen.nl>
2026-06-17 21:51:00 +0200

    game maak pagina geupdate en wat models aangemaakt

--- resources/views/gameview.blade.php
+++ resources/views/gameview.blade.php
@@ -1,1 +1,11 @@
-<div>halloo game inzien</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12 flex flex-col gap-6 text-white">
+        games inzien
+    </div>
+</x-app-layout>
